0afb52075d1a26526e249e201062bfaa41d629ff
aceinetx <mouselin82@gmail.com>
2025-08-21 10:14:49 +0000

    Regen docs

--- docs/classstar_1_1ActionManager.js
+++ docs/classstar_1_1ActionManager.js
@@ -1,6 +1,7 @@
 var classstar_1_1ActionManager =
 [
     [ "removeAllActionsFromTarget", "classstar_1_1ActionManager.html#a09444b573711b9389e460f9a71837299", null ],
+    [ "removeAllDoneActions", "classstar_1_1ActionManager.html#a30fa418c868fbad27378424823eb4f3b", null ],
     [ "runActionForTarget", "classstar_1_1ActionManager.html#ad289a059cfb3d67cf3bc4bbff33a5e2d", null ],
     [ "stepActions", "classstar_1_1ActionManager.html#a8c0a3d28cbe76858841d2ca2c888822a", null ]
 ];
